31f4c58112f04f32424fab65d7e6ed3ea87dad4d
Ryan <tamielryan@gmail.com>
2022-04-16 22:46:13 +0300

    ModelCreated

---
+++ PhotosApp/Model/PhotoSize.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoSize.swift
+//  PhotosApp
+//
+//  Created by Ryan J. W. Kim on 2022/04/16.
+//
+
+import Foundation
+
+struct PhotoSize: Codable {
+    var landscape: String
+    var portrait: String
+}
